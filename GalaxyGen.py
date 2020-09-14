4b7c49ef2a65ac03e79f77220d3873233ae6d29b
root <root@Quontex-Desk.localdomain>
2020-09-14 23:53:27 +1000

    Commit 1

---
+++ GalaxyGen.py
@@ -0,0 +1,57 @@
+import random
+import datetime
+
+class GalaxyGen:
+
+    seed: int
+    name: str
+
+    def galaxyGenerator(self) -> None:
+        """
+        Where all the data comes together to formulate and generate the larger axy
+        """
+        pass
+
+
+
+    def starGenerate(self) -> None:
+        """
+        Function to generate either a specific class or generic rtandom star
+        """
+        pass
+    
+    
+    
+    def planetGenerator(self) -> None:
+        """
+        Function to generate either a specific type or generic rtandom planet
+        """
+        pass
+    
+    
+    
+    def moonGenerator(self) -> None:
+        """
+        Copy of planetGenerator with extra logic to determin moons
+        """
+        pass
+    
+    
+    
+    def asteroidGenerator(self) -> None:
+        """
+        Function to generate a generic rtandom asteroid
+        """
+        pass
+    
+    
+    
+    def planemoGenerator(self) -> None:
+        """
+        Function to generate rouge planets without a solar system in insterstellar space
+        """
+        pass
+
+if __name__ == "__main__":
+
+    print(f"This is a lib file. Not to be run on it's own.")
